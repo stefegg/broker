12e8464ce322fe009bdcc1aaac52b040efe25bcd
stefegg <stephenegbert@gmail.com>
2018-12-06 13:41:56 -0500

    changes

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
+const stream = require('stream');
 
 
 const listener = require('../lib/listenMode.js');
 const clientUpload = require('../lib/uploadMode.js');
+
 
 function cliInput() {
     if (process.argv[2] === 'listen') {
@@ -10,6 +12,7 @@ function cliInput() {
     } else if (process.argv[2] === 'upload') {
         let file = process.argv[3]
         let fileName = file.substring(file.lastIndexOf("=") + 1);
+        
         clientUpload.clientUpload(fileName)
         // console.log(fileName)
     } else console.log('Incorrect command, see readme')
